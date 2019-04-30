1b9ef5dbe7f83984671fbb5f694b65332f411a7e
D.K <alukardev@gmail.com>
2019-04-30 14:09:09 +0300

    test

---
+++ Exstensions/Assets/Editor/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class HelloWorld
+    {
+        [MenuItem("GameObject/Create Hello World!")]
+        private static void CreateHelloWorldObject()
+        {
+            if (EditorUtility.DisplayDialog("Hello World", "Do you really want to do this?", "OK", "Cancel"))
+            {
+                new GameObject("Hello World");
+            }
+        }
+    }
+}
